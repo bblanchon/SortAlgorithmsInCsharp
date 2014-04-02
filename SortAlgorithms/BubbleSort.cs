d1fa80cda75044f5b4e071255036255d55c6924a
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-04-02 22:17:24 +0200

    Started with bubblesort

---
+++ SortAlgorithms/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortAlgorithms
+{
+    public class BubbleSort<T>
+         where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            var n = array.Length;
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = i; j < n; j++)
+                {
+                    SwapIfNeeded(array, i, j);
+                }
+            }
+        }
+
+        static void SwapIfNeeded(T[] array, int i, int j)
+        {
+            if (array[i].CompareTo(array[j]) <= 0) return;
+            Swap(array, i, j);
+        }
+
+        static void Swap(T[] array, int i, int j)
+        {
+            var tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
---
+++ SortAlgorithmsTests/BubbleSortTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortAlgorithms;
+
+namespace SortAlgorithmsTests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        BubbleSort<int> algo;
+        Random rand;
+        Stopwatch chrono;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            algo = new BubbleSort<int>();
+            rand = new Random();
+            chrono = new Stopwatch();
+        }
+
+        [TestMethod]
+        public void EmptyArray()
+        {
+            RunTest(/*empty*/);
+        }
+
+        [TestMethod]
+        public void OneElement()
+        {
+            RunTest(42);
+        }
+
+        [TestMethod]
+        public void TwoElementsUnordered()
+        {
+            RunTest(42, 41);
+        }
+
+        [TestMethod]
+        public void TwoElementsOrdered()
+        {
+            RunTest(41, 42);
+        }
+
+        [TestMethod]
+        public void Random100Elements()
+        {
+            RunTest(GenerateRandomArray(100));
+        }
+
+        [TestMethod]
+        public void Random1000Elements()
+        {
+            RunTest(GenerateRandomArray(1000));
+        }
+
+        [TestMethod]
+        public void Random10000Elements()
+        {
+            RunTest(GenerateRandomArray(10000));
+        }
+
+        int[] GenerateRandomArray(int length)
+        {
+            return Enumerable.Range(0, length).Select(x => rand.Next()).ToArray();
+        }
+        
+        void RunTest(params int[] input)
+        {
+            var expected = input.OrderBy(x => x).ToArray();
+            RunAlgorithm(input);
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        void RunAlgorithm(int[] input)
+        {
+            chrono.Start();
+            algo.Sort(input);
+            chrono.Stop();
+
+            Console.WriteLine("{0} elements sorted in {1} ms", input.Length, chrono.ElapsedMilliseconds);
+        }
+    }
+}
